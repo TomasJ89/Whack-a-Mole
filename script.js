10ae351ffad7664c985cb7528e79456491987b09
unknown <tomasjarutis@gmail.com>
2024-05-15 10:54:58 +0300

    js update

---
+++ script.js
@@ -0,0 +1,40 @@
+const gameContainer = document.querySelector(".gameContainer");
+const pointsHTML = document.querySelector("h2");
+const cellsArr = [
+    1, 2, 3, 4, 5,
+    1, 2, 3, 4, 5,
+    1, 2, 3, 4, 5,
+    1, 2, 3, 4, 5,
+    1, 2, 3, 4, 5,
+];
+
+let points = 0;
+
+// Function to set the background color of a random cell to red
+function turnRandomCellRed() {
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.classList.remove("red");
+    });
+    let rndNum = Math.floor(Math.random() * cellsArr.length);
+    const cell = document.querySelectorAll('.cell')[rndNum];
+    cell.classList.add("red");
+}
+
+// Function to handle cell click
+function handleCellClick(event) {
+    if (event.target.classList.contains("red")) {
+        points++;
+        pointsHTML.textContent = `Points: ${points}`;
+    }
+}
+
+// Create cells
+cellsArr.forEach((cell, index) => {
+    const div = document.createElement("div");
+    div.className = "cell";
+    div.addEventListener("click", handleCellClick);
+    gameContainer.appendChild(div);
+});
+
+setInterval(turnRandomCellRed, 1000);
